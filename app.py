b982a99bd8c38ef76472bcc36a9396efa0a8dddc
Thimod Prabasha <thimo@Thimods-MacBook-Pro.local>
2023-12-25 14:35:07 +0530

    added model

--- app.py
+++ app.py
@@ -1,83 +1,81 @@
 from flask import Flask
 from flask_cors import CORS
 import os
-
-from flask import Flask, request, json
-from werkzeug.utils import secure_filename
-# from dectection import engine
+from functools import wraps
+from flask import Flask, request, json, jsonify
+from fake_real_news_dectector import detect_real_or_fake_news
+from werkzeug.exceptions import BadRequest
+from jsonschema import validate, ValidationError
 
 app = Flask(__name__)
 cors = CORS(app)
 
 
-
-# def check_path_exists():
-#     if not os.path.exists(app.config['UPLOAD_FOLDER']):
-#        os.makedirs(app.config['UPLOAD_FOLDER'])
-
-
 def response_config(data, status_code, mime_type):
     return app.response_class(
-        response=json.dumps(data),
-        status=status_code,
-        mimetype=mime_type
+        response=json.dumps(data), status=status_code, mimetype=mime_type
     )
 
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# def remove_temp_files():
-#     if not os.listdir(uploads) == []:
-#         files = [f for f in os.listdir(uploads) if os.path.isfile(os.path.join(uploads, f))]
-#         for file in files:
-#             os.remove(os.path.join(uploads, file))
+app.config["JSON_SCHEMA"] = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "id": {"type": "number", "minimum": 0},
+            "text": {"type": "string", "minLength": 1},
+            "class": {"type": "string"},
+        },
+        "required": ["id", "text"],
+    },
+}
 
 
+def validate_json(f):
+    @wraps(f)
+    def wrapper(*args, **kw):
+        try:
+            request.json
+        except BadRequest as e:
+            data = {"Message": "400 Bad Request: Payload must be a valid json"}
+            return response_config(data, 400, "application/json")
+        return f(*args, **kw)
 
-@app.route('/api/detect-news-validation', methods=['POST'])
+    return wrapper
+
+
+def validate_schema(schema_name):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kw):
+            try:
+                validate(request.json, app.config[schema_name])
+            except ValidationError as e:
+                data = {"Message": str(e.message)}
+                return response_config(data, 400, "application/json")
+            return f(*args, **kw)
+
+        return wrapper
+
+    return decorator
+
+
+@app.route("/api/detect-news-validation", methods=["POST"])
+@validate_json
+@validate_schema("JSON_SCHEMA")
 def get_text():
+    try:
+        content = request.json
 
-    # try:
-    #     if 'file' not in request.files:
-    #         data = {
-    #             'Message': 'Image not found!'
-    #         }
-    #         return response_config(data, 404, 'application/json')
+        res = detect_real_or_fake_news(content)
 
-    #     for image in request.files.getlist('file'):
-    #         if image.filename == '':
-    #             data = {
-    #                 'Message': 'No selected image!'
-    #             }
-    #             return response_config(data, 404, 'application/json')
+        return response_config(res, 200, "application/json")
 
-    #         if image and allowed_file(image.filename):
-    #                 filename = secure_filename(image.filename)
-    #                 temp_image = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    #                 image.save(temp_image)
-            
-    #         else:
-    #             data = {
-    #                 'Message': 'Invalid image type!'
-    #             }
-    #             return response_config(data, 404, 'application/json')
-
-    #     # res = engine(app.logger)
-
-    #     remove_temp_files()
-    #     return response_config(res, 200, 'application/json')
-    
-    # except Exception as e:
-    #     app.logger.error('Error : ', str(e))
-    #     data = {
-    #         'Message': str(e)
-    #         }
-    #     remove_temp_files()
-    #     return response_config(data, 505, 'application/json')
+    except Exception as e:
+        app.logger.error("Error : ", str(e))
+        data = {"Message": str(e)}
+        return response_config(data, 500, "application/json")
 
 
-if __name__ == '__main__':
-    # check_path_exists()
-    # remove_temp_files()
+if __name__ == "__main__":
     app.run(port=8000, debug=False)
--- fake_real_news_dectector.py
+++ fake_real_news_dectector.py
@@ -1,13 +1,59 @@
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification , BertTokenizer
+import os
+from transformers import TFAutoModelForSequenceClassification, AutoTokenizer
+import numpy as np
+import json
+from tqdm import tqdm
 
 
+model_path = "model"
+CLASS_NAMES = ["Fake", "Real"]
+mode_file = ["config.json", "tf_model.h5"]
+bert_name = "bert-base-uncased"
 
 
+def load_test_data():
+    with open("data/test_data.json", "r") as f:
+        return json.load(f)
 
 
+def detect_real_or_fake_news(data):
+    if os.path.exists(model_path):
+        for file in mode_file:
+            if not os.path.isfile(os.path.join(model_path, file)):
+                raise FileNotFoundError(f"No such model file : '{file}'")
+    else:
+        raise FileNotFoundError(f"No such model directory : '{model_path}'")
+
+    return_objs = {
+        "result": [],
+    }
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        bert_name,
+        padding="max_length",
+        do_lower_case=True,
+        add_special_tokens=True,
+    )
+
+    model = TFAutoModelForSequenceClassification.from_pretrained("model")
+
+    for _, data in tqdm(enumerate(data)):
+        tokens = tokenizer(
+            data["text"], return_tensors="tf", padding="max_length", truncation=True
+        ).input_ids
+
+        pred = np.abs(np.round(model.predict(tokens, verbose=0).logits))
+
+        result_data = {
+            "id": data["id"],
+            "text": data["text"],
+            "predicted_class": CLASS_NAMES[int(pred)],
+        }
+
+        return_objs["result"].append(result_data)
+    return return_objs
 
 
-
-
-def detect_real_or_fake_news(text : str):
-    
+if __name__ == "__main__":
+    data = load_test_data()
+    detect_real_or_fake_news(data)
